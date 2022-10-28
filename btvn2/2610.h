4208acf0d16f09221f6d63144299c649869f77b0
Trandat231104 <115758394+Trandat231104@users.noreply.github.com>
2022-10-28 08:17:42 +0700

    btvn 2610

---
+++ btvn2/2610.h
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int chuvitamgiac(int a, int b, int c){
+	int s=a+b+c;
+	return s
+}
+// bai 2
+int stamgiac(int a, int b, int c){
+	int q (a+b+c/2);
+	float s=(1.0/4)*sqrt((a+b+c)*(a+b-c)*(b+c-a)*(c+a-b));
+	return s
+
+}
+//3
+int tongchuso1songuyen(int n){
+	int x+=sd;
+	while(sd%10);
+	return x
+
+}
+//4
+int uocchunglonnhat(int a,int b){
+	int i=0;
+	int b-1;
+	while(a%i==0&&b%i==0);
+	return i;
+}
+//5
+int boichungnhonhat(int a , int b){
+	int i=0;
+	int a+1;
+	while(a%i==0&&b%i==0);
+	return i;
+}
+//5
+int sochinhphuong(int n){
+	int i=0;
+	while(n%i==0);
+	n%i=i;
+	i++;
+	return i
+}
+//6
+int sochinhphuong(int n){
+	int i;
+	while(sqrt(n)==i);
+	if ((i*i)==n);
+	i++;
+	return n
+	//7
+	int songuyennhonhat(int n=1,s=0){
+while(s<10000){
+	s=s+n;
+	n++;
+}
+printf("\nTong la %d" ,s);
+printf("\nSo la la %d" ,n)
+}
